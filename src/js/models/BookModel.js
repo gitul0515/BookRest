3acdbfecdce13f62aa5f17bc7e0d57a55fcbd3c9
gitul0515 <gitul0515@gmail.com>
2022-04-13 22:40:18 +0900

    Feat: 로컬 스토리지 추가

--- src/js/models/BookModel.js
+++ src/js/models/BookModel.js
@@ -1,16 +1,21 @@
-export default {
-  data: [
-    {
-      title: "사피엔스",
-      authors: ['유발 하라리'],
-      publisher: "김영사",
-      datetime: "2015-11-24T00:00:00.000+09:00",
-      isbn: "8934972467 9788934972464",
-      thumbnail: "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F521598%3Ftimestamp%3D20220411162844",
-      rating: "7"
-    },
-  ],
+import { getItem, setItem } from "../utility/storage.js";
+const BOOK_MODEL_DATA_KEY = 'bookModelDataKey';
 
+const data = getItem(BOOK_MODEL_DATA_KEY, [
+  {
+    title: "사피엔스",
+    authors: ['유발 하라리'],
+    publisher: "김영사",
+    datetime: "2015-11-24T00:00:00.000+09:00",
+    isbn: "8934972467 9788934972464",
+    thumbnail: "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F521598%3Ftimestamp%3D20220411162844",
+    rating: "7"
+  },
+])
+
+export default {
+  data,
+  
   list(query) {
     // const searchResult = this.search(query);
     return new Promise(res => {
@@ -28,6 +33,7 @@ export default {
     return new Promise(res => {
       setTimeout(() => {
         this.data = [...this.data, { ...newItem, rating: "8" }];
+        setItem(BOOK_MODEL_DATA_KEY, this.data);
         res(this.data);
       }, 200);
     });
---
+++ src/js/utility/storage.js
@@ -0,0 +1,26 @@
+const storage = window.localStorage;
+
+export const setItem = (key, value) => {
+  try {
+    storage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.error('localStorage 저장 오류');
+  }
+};
+
+export const getItem = (key, defaultValue) => {
+  try {
+    const storedValue = storage.getItem(key);
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+    return defaultValue;
+  } catch (e) {
+    console.error(e);
+    return defaultValue;
+  }
+};
+
+export const removeItem = (key) => {
+  storage.removeItem(key);
+}
