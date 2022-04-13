c746fee760814115b635fdd3191badaa56f385f9
gitul0515 <gitul0515@gmail.com>
2022-04-13 21:50:01 +0900

    Feat: 평점(rating) 데이터에 따른 html 생성 함수 추가

--- src/js/models/BookModel.js
+++ src/js/models/BookModel.js
@@ -7,7 +7,7 @@ export default {
       datetime: "2015-11-24T00:00:00.000+09:00",
       isbn: "8934972467 9788934972464",
       thumbnail: "https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F521598%3Ftimestamp%3D20220411162844",
-      rating: "5"
+      rating: "7"
     },
   ],
 
@@ -16,7 +16,7 @@ export default {
     return new Promise(res => {
       setTimeout(()=> {
         res(this.data);
-      }, 200);
+      }, 0);
     })
   },
 
--- src/js/views/BookListView.js
+++ src/js/views/BookListView.js
@@ -32,15 +32,34 @@ BookListView.getHtml = function(list) {
   `).join('');
 }
 
+/* 평점(rating)에 따라 html을 다르게 생성한다. 
+ * 예1: 평점이 8인 경우, 노란색 별 아이콘 4개를 생성하고
+ *     색깔 없는 별 아이콘 1개를 생성한다. 
+ * 예2: 평점이 5인 경우, 노란색 별 아이콘 2개를 생성하고
+ *     반쪽 별 아이콘 1개를 생성하고, 색깔 없는 별 아이콘 2개를 생성한다. 
+ */
 BookListView.getRatingHtml = function(_rating) {
+  let html = '';
   const rating = parseInt(_rating, 10);
-  console.log(rating);
-
-  return `<li><i class="fa-solid fa-star"></i></li>
-  <li><i class="fa-solid fa-star"></i></li>
-  <li><i class="fa-solid fa-star"></i></li>
-  <li><i class="fa-solid fa-star"></i></li>
-  <li><i class="fa-solid fa-star"></i></li>`
+  if (rating % 2 === 0) {
+    const starCount = rating / 2;
+    for (let i = 0; i < starCount; i++) {
+      html += '<li><i class="fa-solid fa-star"></i></li>';
+    }
+    for (let i = 0; i < 5 - starCount; i++) {
+      html += '<li><i class="fa-regular fa-star"></i></li>';
+    }
+  } else {
+    const starCount = Math.floor(rating / 2);
+    for (let i = 0; i < starCount; i++) {
+      html += '<li><i class="fa-solid fa-star"></i></li>';
+    }
+    html += '<li><i class="fa-solid fa-star-half-stroke"></i></li>';
+    for (let i = 0; i < 4 - starCount; i++) {
+      html += '<li><i class="fa-regular fa-star"></i></li>';
+    }
+  }
+  return html;
 }
 
 export default BookListView;
