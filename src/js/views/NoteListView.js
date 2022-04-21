f0a79e100ace7b4d210af002e257734da801112c
gitul0515 <gitul0515@gmail.com>
2022-04-21 16:59:18 +0900

    Design: NoteListView 마크업 추가 - 미완성

--- src/js/views/NoteListView.js
+++ src/js/views/NoteListView.js
@@ -4,19 +4,38 @@ const NoteListView = Object.create(View);
 
 NoteListView.setup = function (element) {
   this.init(element);
-  console.log('noteList', this.element);
   return this;
 };
 
-NoteListView.render = function (data) {
-  if (!Array.isArray(data) || data.length === 0) {
-    return;
-  }
-  const html = this.getHtml(data);
+NoteListView.render = function () {
+  const html = this.getHtml();
   const element = this.createElement(html);
   this.element.replaceChildren(element);
 };
 
-NoteListView.getHtml = function (data) {};
+NoteListView.getHtml = function () {
+  return `
+    <li class="note-item">
+      <header class="note-item__header">
+        <h3 class="note-item__label">
+          <i class="fa-solid fa-thumbtack"></i>
+          책의 문장
+        </h3>
+        <div class="note-item__information">
+          <div class="note-item__book-informaion">
+            <div>
+              <h3 class="note-item__book-title">딥 워크</h3>
+              <span class="note-item__book-author">칼 뉴포트</span>
+            </div>
+            <img class="note-item__book-thumbnail" src="./asset/image/book/book-1.jpg"/>
+          </div>
+          <p class="note-item__created-at">2022년</p>
+        </div>
+      </header>
+      <section class="note-item__content"></section>
+      <footer class="note-item__footer"></footer>
+    </li>
+  `;
+};
 
 export default NoteListView;
