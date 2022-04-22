e427c479973b4dd6d2adcd1ff21f4ce66c331a49
gitul0515 <gitul0515@gmail.com>
2022-04-22 17:42:20 +0900

    Design: NoteListView 마크업 및 스타일 추가

--- src/js/views/NoteListView.js
+++ src/js/views/NoteListView.js
@@ -19,7 +19,7 @@ NoteListView.getHtml = function () {
       <header class="note-item__header">
         <h3 class="note-item__label">
           <i class="fa-solid fa-thumbtack"></i>
-          책의 문장
+          책 속 문장
         </h3>
         <div class="note-item__information">
           <div class="note-item__book-informaion">
@@ -27,14 +27,70 @@ NoteListView.getHtml = function () {
               <h3 class="note-item__book-title">딥 워크</h3>
               <span class="note-item__book-author">칼 뉴포트</span>
             </div>
-            <img class="note-item__book-thumbnail" src="./asset/image/book/book-1.jpg"/>
+            <img class="note-item__book-thumbnail" src="/src/asset/image/book/book-1.jpg"/>
           </div>
-          <p class="note-item__created-at">2022년</p>
+          <p class="note-item__created-at">2022년 4월 9일 12:20</p>
         </div>
       </header>
-      <section class="note-item__content"></section>
-      <footer class="note-item__footer"></footer>
+      <section class="note-item__content">
+        <p>집중하는 삶이 최선의 삶이다.</p>
+        <span class="note-item__page">p. 137</span>
+      </section>
+      <footer class="note-item__footer">
+        <div class="note-item__btns">
+          <button class="note-item__btn note-item__btn--count">
+            <i class="fa-solid fa-check"></i>
+          </button>
+          <button class="note-item__btn note-item__btn--favorite">
+            <i class="fa-regular fa-heart"></i>
+          </button>
+          <button class="note-item__btn note-item__btn--comment">
+            <i class="fa-regular fa-comment"></i>
+          </button>
+          <button class="note-item__btn note-item__btn--options">
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+          </button>
+        </div>
+      </footer>
     </li>
+    <li class="note-item">
+    <header class="note-item__header">
+      <h3 class="note-item__label">
+        <i class="fa-solid fa-thumbtack"></i>
+        책 속 문장
+      </h3>
+      <div class="note-item__information">
+        <div class="note-item__book-informaion">
+          <div>
+            <h3 class="note-item__book-title">딥 워크</h3>
+            <span class="note-item__book-author">칼 뉴포트</span>
+          </div>
+          <img class="note-item__book-thumbnail" src="/src/asset/image/book/book-1.jpg"/>
+        </div>
+        <p class="note-item__created-at">2022년 4월 9일 12:20</p>
+      </div>
+    </header>
+    <section class="note-item__content">
+      <p>딥 워크</p>
+      <span class="note-item__page">p. 137</span>
+    </section>
+    <footer class="note-item__footer">
+      <div class="note-item__btns">
+        <button class="note-item__btn note-item__btn--count">
+          <i class="fa-solid fa-check"></i>
+        </button>
+        <button class="note-item__btn note-item__btn--favorite">
+          <i class="fa-regular fa-heart"></i>
+        </button>
+        <button class="note-item__btn note-item__btn--comment">
+          <i class="fa-regular fa-comment"></i>
+        </button>
+        <button class="note-item__btn note-item__btn--options">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </button>
+      </div>
+    </footer>
+  </li>
   `;
 };
 
