ab3379f42100e46e6134952ee2933dce49a39720
gitul0515 <gitul0515@gmail.com>
2022-04-26 22:20:03 +0900

    refactor: #app 하위 태그로 #page, #navigation 이동

--- src/scripts/main/controllers/MainController.js
+++ src/scripts/main/controllers/MainController.js
@@ -3,52 +3,54 @@ import HomePageView from '../views/HomePageView.js';
 import NotePageView from '../views/NotePageView.js';
 import BookPageView from '../views/BookPageView.js';
 import SettingView from '../views/SettingView.js';
+
 import HomeController from './HomeController.js';
 import BookController from './BookController.js';
 import NoteController from './NoteController.js';
 import SettingController from './SettingController.js';
 
-const app = document.getElementById('app');
+const page = document.getElementById('page');
 const navigation = document.getElementById('navigation');
 
 export default {
   init() {
-    this.currentPage = '/note';
-    this.switchPage();
-    window.addEventListener('popstate', () => this.onPopstate());
-
-    NavigationView.setup(navigation).on('@click', (e) =>
-      this.onClick(e.detail.page)
-    );
-  },
-
-  onClick(page) {
-    this.currentPage = page;
-    this.switchPage();
+    this.currentPage = '/';
     history.pushState(null, null, this.currentPage);
+    this.switchPage();
+
+    NavigationView.setup(navigation) //
+      .on('@click', (e) => this.onClick(e.detail.page));
+
+    window.addEventListener('popstate', () => this.onPopstate());
   },
 
   switchPage() {
     switch (this.currentPage) {
       case '/':
-        HomePageView.setup(app);
+        HomePageView.setup(page);
         HomeController.init();
         break;
       case '/book':
-        BookPageView.setup(app);
+        BookPageView.setup(page);
         BookController.init();
         break;
       case '/note':
-        NotePageView.setup(app);
+        NotePageView.setup(page);
         NoteController.init();
         break;
       case '/setting':
-        SettingView.setup(app);
+        SettingView.setup(page);
         SettingController.init();
         break;
       default:
         throw new Error('invalid page');
     }
+  },
+
+  onClick(page) {
+    this.currentPage = page;
+    history.pushState(null, null, this.currentPage);
+    this.switchPage();
   },
 
   onPopstate() {
--- src/scripts/main/views/NavigationView.js
+++ src/scripts/main/views/NavigationView.js
@@ -2,27 +2,36 @@ import View from './View.js';
 
 const NavigationView = Object.create(View);
 
-NavigationView.setup = function(element) {
+NavigationView.setup = function (element) {
   this.init(element);
+  this.render();
   this.bindEvent();
   return this;
-}
+};
 
-NavigationView.bindEvent = function() {
-  this.element.addEventListener('click', e => this.onClick(e));
+NavigationView.render = function () {
+  this.element.innerHTML = `
+    <a href="/"><i class="fa-solid fa-plus"></i></a>
+    <a href="/book"><i class="fa-solid fa-book-open-reader"></i></a>
+    <a href="/note"><i class="fa-solid fa-pen-to-square"></i></a>
+    <a href="/setting"><i class="fa-solid fa-square-poll-horizontal"></i></a
+  `;
+};
+
+NavigationView.bindEvent = function () {
+  this.element.addEventListener('click', (e) => this.onClick(e));
 };
 
 /* 
   a 태그를 클릭하면 페이지를 전환한다. 
   클릭 이벤트가 발생했음을 MainController에게 알린다.  
 */
-NavigationView.onClick = function(e) {
-  if (!e.target.matches('#navigation > a')) {
-    return;
+NavigationView.onClick = function (e) {
+  if (e.target.matches('#navigation > a')) {
+    e.preventDefault();
+    const page = e.target.getAttribute('href');
+    this.emit('@click', { page });
   }
-  e.preventDefault();
-  const page = e.target.getAttribute('href');
-  this.emit('@click', { page });
 };
 
 export default NavigationView;
