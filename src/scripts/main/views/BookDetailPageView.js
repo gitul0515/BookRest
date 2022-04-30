f3c5ee5f107c4604ad3f072fc2a3cb4e586d82b5
gitul0515 <gitul0515@gmail.com>
2022-04-30 22:50:51 +0900

    design: BookDetailPageView 헤더 영역 마크업 추가

--- src/scripts/main/views/BookDetailPageView.js
+++ src/scripts/main/views/BookDetailPageView.js
@@ -17,7 +17,29 @@ BookDetailPageView.render = function () {
 };
 
 BookDetailPageView.getHtml = function () {
-  return `<h1>hello</h1>`;
+  return `
+    <header class="detail-page__header">
+      <div class="detail-page__btns"> 
+        <button class="detail-page__btn detail-page__btn--prev">
+          <i class="fa-solid fa-arrow-left"></i>
+        </button>
+        <button class="detail-page__btn detail-page__btn--menu">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </button>
+      </div>
+      <h1 class="detail-page__title">데미안</h1>
+      <div class="detail-page__informaion">
+        <div class="detail-page__thumbnail">
+          <img src="/src/asset/image/book/book-4.jpg" alt="썸네일 이미지"/>
+        </div>
+        <div class="detail-page__description">
+          <span class="detail-page__authors">헤르만 헤세</span>
+          <span class="detail-page__publisher">민음사</span>
+        </div>
+      </div>
+    </header>
+    <ul class="detail-page__note-list"></ul>
+  `;
 };
 
 BookDetailPageView.bindElement = function () {};
