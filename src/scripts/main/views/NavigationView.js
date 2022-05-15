3e0a95290a2413eebaa65b3d7a65a4e801d8191e
gitul0515 <gitul0515@gmail.com>
2022-05-15 14:58:19 +0900

    design: navigationView 탭 전환 효과 추가

--- src/scripts/main/views/NavigationView.js
+++ src/scripts/main/views/NavigationView.js
@@ -1,25 +1,39 @@
 import View from './View.js';
+import { addClass, removeClass } from '../../utils/className.js';
 
 const NavigationView = Object.create(View);
 
 NavigationView.setup = function (element) {
   this.init(element);
   this.render();
-  this.bindEvent();
+  this.setEvent();
+  this.currentTab = this.element.firstElementChild;
   return this;
 };
 
 NavigationView.render = function () {
-  this.element.innerHTML = /* html */ 
-  `
-    <a href="/"><i class="fa-solid fa-plus"></i></a>
-    <a href="/book"><i class="fa-solid fa-book-open-reader"></i></a>
-    <a href="/note"><i class="fa-solid fa-pen-to-square"></i></a>
-    <a href="/setting"><i class="fa-solid fa-square-poll-horizontal"></i></a>
+  const html = this.getHtml();
+  this.replaceChildren(html);
+};
+
+NavigationView.getHtml = function () {
+  return /* html */ `
+    <a class="navigation__tab-link active" href="/">
+      <i class="navigation__tab-icon fa-solid fa-house"></i>
+    </a>
+    <a class="navigation__tab-link" href="/book">
+      <i class="navigation__tab-icon fa-solid fa-book-open-reader"></i>
+    </a>
+    <a class="navigation__tab-link" href="/note">
+      <i class="navigation__tab-icon fa-solid fa-pen-to-square"></i>
+    </a>
+    <a class="navigation__tab-link" href="/setting">
+      <i class="navigation__tab-icon fa-solid fa-square-poll-horizontal"></i>
+    </a>
   `;
 };
 
-NavigationView.bindEvent = function () {
+NavigationView.setEvent = function () {
   this.element.addEventListener('click', (e) => this.onClick(e));
 };
 
@@ -28,11 +42,18 @@ NavigationView.bindEvent = function () {
   클릭 이벤트가 발생했음을 MainController에게 알린다.  
 */
 NavigationView.onClick = function (e) {
-  if (e.target.matches('#navigation > a')) {
+  if (e.target.matches('.navigation__tab-link')) {
     e.preventDefault();
     const page = e.target.getAttribute('href');
     this.dispatch('@click', { page });
+    this.setTabStyle(e.target);
   }
+};
+
+NavigationView.setTabStyle = function (nextTab) {
+  removeClass(this.currentTab, 'active');
+  this.currentTab = nextTab;
+  addClass(this.currentTab, 'active');
 };
 
 NavigationView.show = function () {
