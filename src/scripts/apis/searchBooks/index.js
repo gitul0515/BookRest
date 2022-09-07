b7bdc694097bc02e3d7ae8a86df08c82e62fd42b
gitul0515 <gitul0515@gmail.com>
2022-09-08 01:35:16 +0900

    fix: API_END_POINT 수정

--- src/scripts/apis/searchBooks/index.js
+++ src/scripts/apis/searchBooks/index.js
@@ -1,8 +1,8 @@
-import { API_END_POINT, API_KEY } from './config.js';
+import { API_KEY } from './config.js';
 
 export async function searchBooks(searchWord, page) {
   const res = await fetch(
-    `${API_END_POINT}?sort=accuracy&page=${page}&size=10&query=${searchWord}`,
+    `https://dapi.kakao.com/v3/search/book?sort=accuracy&page=${page}&size=10&query=${searchWord}`,
     {
       headers: {
         Authorization: `KakaoAK ${API_KEY}`,
