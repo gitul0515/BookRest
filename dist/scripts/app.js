71c6f93fdfbb4645489a5c12f01fa4444ee11153
gitul0515 <gitul0515@gmail.com>
2022-09-24 16:32:25 +0900

    remove: babel 단독으로 만든 dist 폴더 삭제

--- dist/scripts/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _MainController = _interopRequireDefault(require("./main/controllers/MainController.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-document.addEventListener('DOMContentLoaded', function () {
-  _MainController["default"].init();
-});
